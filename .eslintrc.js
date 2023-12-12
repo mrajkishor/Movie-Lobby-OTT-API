d015a477a77480504d1249defbe64b3fe8967671
mrajkishor <148688864+mrajkishor@users.noreply.github.com>
2023-12-12 23:48:50 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,12 +9,8 @@ module.exports = {
     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
     rules: {
         '@typescript-eslint/explicit-module-boundary-types': 'off', // Allowing explicit return types
-        '@typescript-eslint/no-explicit-any': 'off', // Allowing the use of 'any'
         '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }], // Ignore unused variables starting with an underscore
-        'no-console': 'error', // Disallow the use of console.log
         'no-debugger': 'error', // Disallow the use of debugger
-        'no-warning-comments': ['warn', { terms: ['todo', 'fixme', 'xxx'], location: 'start' }], // Warn on specific comment terms
-
-        // Add more rules as needed based on your coding standards
+        'no-warning-comments': ['warn', { terms: ['todo', 'fixme', 'xxx', '// '], location: 'start' }], // Warn on specific comment terms
     },
 };
